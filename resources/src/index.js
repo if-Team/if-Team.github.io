6004720d1ab8c2d3f2916a6a8ce857053708aeee
ChalkPE <chalkpe@gmail.com>
2016-02-29 21:47:04 +0900

    Added contents

--- resources/src/index.js
+++ resources/src/index.js
@@ -1,4 +1,8 @@
-console.log("Hello, if-Team!");
 $(document).ready(function(){
     $('.parallax').parallax();
+    $('#particles').particleground({
+        dotColor: '#eee',
+        lineColor: '#eee',
+        density: 7000
+    });
 });
